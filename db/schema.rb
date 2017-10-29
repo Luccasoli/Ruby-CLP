4414c373444051770d0a96c536b792bb77901d50
luccasoli <lucasoliveira98@outlook.com>
2017-10-29 12:49:04 -0300

    Refazendo pra aprender

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028224824) do
+ActiveRecord::Schema.define(version: 20171029154736) do
 
   create_table "clients", force: :cascade do |t|
     t.string "nome"
